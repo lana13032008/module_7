09b60076d53c0bbdff9493bec2c7cb0c95b0cb65
Lana <lananiyazbek3@gmail.com>
2025-06-06 12:48:19 +0500

    7.2

---
+++ script_2.js
@@ -0,0 +1,77 @@
+let age = 25;
+console.log(age); 
+
+age = 30;
+console.log(age);  
+
+const birthYear = 2000;
+console.log(birthYear);
+
+
+
+let numberExample = 42;
+ console.log(numberExample, typeof numberExample); 
+
+
+let stringExample = "Hello, world!";
+ console.log(stringExample, typeof stringExample); 
+
+        
+let booleanExample = true;
+console.log(booleanExample, typeof booleanExample); 
+
+    
+let nullExample = null;
+console.log(nullExample, typeof nullExample);  
+
+
+let undefinedExample;
+console.log(undefinedExample, typeof undefinedExample);  
+
+
+let nanExample = NaN;
+console.log(nanExample, typeof nanExample);
+
+
+let str = "123";
+let strToNumber = Number(str);
+console.log(strToNumber, typeof strToNumber);
+
+let num = 456;
+let numToString = String(num);
+console.log(numToString, typeof numToString);
+
+let undef = undefined;
+let undefToString = String(undef);
+console.log(undefToString, typeof undefToString);
+
+let undefToNumber = Number(undef);
+console.log(undefToNumber, typeof undefToNumber);
+
+let nullValue = null;
+let nullToString = String(nullValue);
+console.log(nullToString, typeof nullToString);
+
+let nullToNumber = Number(nullValue);
+console.log(nullToNumber, typeof nullToNumber);
+
+
+
+
+
+
+let variable = 100;
+console.log(variable, typeof variable);
+
+variable = "Теперь это строка";
+console.log(variable, typeof variable);
+
+
+
+
+
+let name = prompt("Введите ваше имя:");
+let age_task6 = prompt("Введите ваш возраст:");
+
+console.log("Имя:", name);
+console.log("Возраст:", age_task6);
